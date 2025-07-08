627c36db0b3e5ad750c1a6004a83fb3c3307753f
CodewithAvijit <codewithavijit@gmail.com>
2025-07-08 18:10:57 +0530

    deployable version

--- FARMPLANNER/start.sh
+++ FARMPLANNER/start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Activate the virtual environment
-source ../AGROENV/Scripts/activate
-
-# Run the FastAPI app
-uvicorn main:app --host 127.0.0.1 --port 10000
+uvicorn main:app --host 0.0.0.0 --port 10000
